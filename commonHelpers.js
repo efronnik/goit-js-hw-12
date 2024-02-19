0ad2994f5b984a57c5888bc47fff7771b13ca3d9
efronnik <93129217+efronnik@users.noreply.github.com>
2024-02-19 02:35:31 +0000

    Deploying to gh-pages from @ efronnik/goit-js-hw-12@bf92266f323947e4cb5b9f6fe77d24ffde47ee3b 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,14 +1,14 @@
-import{a as c,S as u,i as m}from"./assets/vendor-64b55ca9.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const a of s.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(e){if(e.ep)return;e.ep=!0;const s=r(e);fetch(e.href,s)}})();const d="gallery-link";function g({largeImageURL:i,tags:t,webformatURL:r,likes:o,views:e,comments:s,downloads:a}){return`
-    <a href="${i}" class="${d}">
+import{a as c,S as m,i as d}from"./assets/vendor-64b55ca9.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const u="gallery-link";function g({largeImageURL:a,tags:r,webformatURL:s,likes:i,views:e,comments:t,downloads:o}){return`
+    <a href="${a}" class="${u}">
       <figure>
-        <img src="${r}" alt="${t}" class="gallery-image">
+        <img src="${s}" alt="${r}" class="gallery-image">
         <figcaption class="gallery__figcaption">
-          <div class="image-item">Likes <span class="image-elem">${o}</span></div>
+          <div class="image-item">Likes <span class="image-elem">${i}</span></div>
           <div class="image-item">Views <span class="image-elem">${e}</span></div>
-          <div class="image-item">Comments <span class="image-elem">${s}</span></div>
-          <div class="image-item">Downloads <span class="image-elem">${a}</span></div>
+          <div class="image-item">Comments <span class="image-elem">${t}</span></div>
+          <div class="image-item">Downloads <span class="image-elem">${o}</span></div>
         </figcaption>
       </figure>
     </a>
-  `}const f=document.querySelector("#gallery"),n=document.querySelector(".search-form"),l=document.querySelector(".loader"),p=document.querySelector(".no-results-message");n.addEventListener("submit",async function(i){i.preventDefault();const t=i.target.elements.query.value;if(t!==""){l.style.display="block";try{const r=await c.get(`https://api.unsplash.com/search/photos?query=${t}&client_id=42404284-d1db8811507a6ab98b0e3f497`),{results:o}=r.data;if(o.length>0){const e=o.map(g).join("");f.innerHTML=e,new u(`.${GALLERY_LINK}`).refresh()}else p.style.display="block"}catch(r){console.error("Error fetching images:",r),m.error({title:"Error",message:`Error fetching images: ${r.message}`,position:"topRight"})}finally{l.style.display="none",n.reset()}}});
+  `}const f=document.querySelector("#gallery"),n=document.querySelector(".search-form"),l=document.querySelector(".loader"),p=document.querySelector(".no-results-message");n.addEventListener("submit",async function(a){a.preventDefault();const r=a.target.elements.query.value;if(r!==""){l.style.display="block";try{const s=await c.get(`https://pixabay.com/api/?key=42404284-d1db8811507a6ab98b0e3f497&q=${r}`);if(s.data.hits&&Array.isArray(s.data.hits)&&s.data.hits.length>0){const i=s.data.hits.map(g).join("");f.innerHTML=i,new m(`.${GALLERY_LINK}`).refresh()}else p.style.display="block"}catch(s){console.error("Error fetching images:",s),d.error({title:"Error",message:`Error fetching images: ${s.message}`,position:"topRight"})}finally{l.style.display="none",n.reset()}}});
 //# sourceMappingURL=commonHelpers.js.map
